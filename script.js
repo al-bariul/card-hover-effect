e36bf980a12343b54c09693a39275befc0ce4dcf
al-bariul <albariul475@gmail.com>
2025-11-28 02:11:33 +0600

    project done

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 const card = document.querySelectorAll(".card");
-console.log(card);
+
+for (const c of card) {
+  c.addEventListener("click", () => {
+    removeActive();
+    // for (const cards of card) {
+    //   console.log(cards);
+    // }
+
+    // Removing done. Now, add the active class.
+    c.classList.add("active");
+  });
+}
+
+// Removing active class from all card. Including the first one where i manually write active class.
+function removeActive() {
+  for (const c of card) {
+    c.classList.remove("active");
+  }
+}
+
+// for (const cards of card) {
+//   console.log(cards);
+// }
